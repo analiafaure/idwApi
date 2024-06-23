0e0772eaf23e40500cde17eca8f31ee0235e3fac
Nacho Novello <50060159+nachonovello1984@users.noreply.github.com>
2024-06-23 12:15:39 -0300

    Update alojamientosServiciosController.js

--- src/controllers/alojamientosServiciosController.js
+++ src/controllers/alojamientosServiciosController.js
@@ -19,7 +19,7 @@ exports.getAlojamientoServicioById = async (req, res) => {
     const connection = await dbConnection.getConnection();
     const [rows] = await connection.query('SELECT * FROM alojamientoservicios WHERE idAlojamientoServicio = ?', [id]);
     connection.release();
-    res.json(rows);
+    res.json(rows[0]);
   } catch (error) {
     res.status(500).json({ error: 'Error al obtener los alojamientoservicios' });
   }
@@ -32,7 +32,7 @@ exports.getAllAlojamientoServiciosByIdAlojamiento = async (req, res) => {
     const connection = await dbConnection.getConnection();
     const [rows] = await connection.query('SELECT * FROM alojamientoservicios WHERE idAlojamiento = ?', [idAlojamiento]);
     connection.release();
-    res.json(rows[0]);
+    res.json(rows);
   } catch (error) {
     console.log(error);
     res.status(500).json({ error: 'Error al obtener el alojamientoservicio' });
